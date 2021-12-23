9ee18242883d9491c8776c6c2e3e216b36269fdc
Olivier Vicario <olvicario@gmail.com>
2021-12-23 09:59:16 +0100

    cours openclassrooms

---
+++ src/test/java/com/openclassrooms/SpringSecurityAuthApplicationTests.java
@@ -0,0 +1,24 @@
+package com.openclassrooms;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.openclassrooms.controller.LoginController;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class SpringSecurityAuthApplicationTests {
+
+	@Autowired
+	private LoginController controller;
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+}
